fc744e6eccd6653986b709c3a89ea46e4cfe14ab
bcblevins <148902357+bcblevins@users.noreply.github.com>
2024-07-10 16:10:53 +0000

    fixing animation 2

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const vetraHeader = document.querySelector('.vetra-header');
 const vetraDescription = document.createElement('p');
 vetraDescription.classList.add('vetra');
 vetraDescription.classList.add('description');
-vetraDescription.classList.add('hide');
 vetraCard.appendChild(vetraDescription);
 vetraDescription.innerText = 
 "VetRA is a web application that streamlines communication between veterinarians and pet owners. It allows pet owners to easily access their pet's diagnostic test results and medications, and allows them to communicate with their vet. VetRA is designed to be user-friendly and intuitive, making it easy for both pet owners and veterinarians to use. \n\nVetRA is a project that I designed back to front, using PostgreSQL and Spring Boot for the backend, and Vue.js for the frontend. It currently supports integration with ezyVet's API, allowing VetRA's database to be automatically updated with patient information from hospitals utilizing ezyVet's practice management software. \n\nMy future plans for VetRA include adding more features, such as appointment scheduling and reminders, and expanding the application to include more types of pets and veterinary clinics. I am excited to continue working on VetRA and to see how it can help improve communication between pet owners and veterinarians."
